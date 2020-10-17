95e1e348d17a4e5716ba83fac4d92d0c80785fcf
Suttekh <josueferrrreira@hotmail.com>
2020-10-17 19:51:46 -0300

    Cheio

---
+++ agua1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Copo d'água</title>
+</head>
+<body>
+<div>
+<h1>To n vei, to cheio</h1>
+   <img src="img/andres" alt=""> 
+   </div>
+</body>
+</html>
